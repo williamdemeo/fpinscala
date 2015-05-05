e9fd359253673adffd1021735667c3987cca1d70
William DeMeo <williamdemeo@gmail.com>
2015-05-05 10:49:03 -0500

    Code is not working yet, but want to commit this version because I'll be trying something totally different.

--- exercises/src/test/scala/fpinscala/datastructures/ListSuite.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ListSuite.scala
@@ -1,10 +1,36 @@
 package fpinscala.datastructures
 
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.prop.Checkers
+import org.scalatest.exceptions.TestFailedException
+
 import org.scalacheck._
 import Arbitrary._
 import Gen._
 import Prop._
 
+object QuickCheckIntList extends QuickCheckList with List[Int]
+
+@RunWith(classOf[JUnitRunner])
+class ListSuite extends FunSuite with Checkers {
+  def checkBogus(p: Prop) {
+    var ok = false
+    try {
+      check(p)
+    } catch {
+      case e: TestFailedException =>
+        ok = true
+    }
+    assert(ok, "A bogus list will NOT satisfy all properties. Try to find the bug!")
+  }
+
+  test("Binomial heap satisfies properties.") {
+    check(new QuickCheckList with List[Int])
+  }
+
+}
 
 
 abstract class QuickCheckList extends Properties("List") {
